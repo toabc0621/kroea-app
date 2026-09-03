3bcff6d6f7f10cf63cbc888fa888e4803772c778
岡村大生 <taiki@okamuradaiseinoMac-mini.local>
2026-09-03 17:39:34 +0900

    Remove navigation tabs

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-// ★ ここに console.log を配置して値が取れているか確認する
-console.log('SUPABASE_URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('SUPABASE_ANON_KEY:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export const supabase = createClient(supabaseUrl, supabaseKey)
